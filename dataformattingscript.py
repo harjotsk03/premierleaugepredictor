48f5a3242333cf24907c83d5aeb0a12af90deb0b
harjotsk03 <harjotsk03@gmail.com>
2024-11-15 14:33:29 -0800

    add the assists data

--- dataformattingscript.py
+++ dataformattingscript.py
@@ -4,134 +4,138 @@ import os
 import csv
 
 raw_data = """Rank	Player	Club	Nationality	Stat	
+
+Rank	Player	Club	Nationality	Stat	
 1.	
-Jamie Vardy
-
-Leicester City
-
-England
-23	 
-2.	
-Pierre-Emerick Aubameyang
--	
-
-Gabon
-22	 
-2.	
-Danny Ings
--	
-
-England
-22	 
-4.	
-Raheem Sterling
--	
-
-England
-20	 
-5.	
-Mohamed Salah
-
-Liverpool
-
-Egypt
-19	 
-6.	
-Harry Kane
--	
-
-England
-18	 
-6.	
-Sadio Mané
--	
-
-Senegal
-18	 
-8.	
-Raúl Jiménez
--	
-
-Mexico
-17	 
-8.	
-Anthony Martial
--	
-
-France
-17	 
-8.	
-Marcus Rashford
-
-Manchester United
-
-England
-17	 
-11.	
-Sergio Agüero
--	
-
-Argentina
-16	 
-12.	
-Tammy Abraham
--	
-
-England
-15	 
-13.	
-Gabriel Jesus
--	
-
-Brazil
-14	 
-13.	
-Chris Wood
--	
-
-New Zealand
-14	 
-15.	
-Dominic Calvert-Lewin
-
-Everton
-
-England
-13	 
-15.	
-Richarlison
--	
-
-Brazil
-13	 
-15.	
 Kevin De Bruyne
 
 Manchester City
 
 Belgium
+20	 
+2.	
+Trent Alexander-Arnold
+
+Liverpool
+
+England
 13	 
-18.	
-Riyad Mahrez
+3.	
+Andy Robertson
+
+Liverpool
+
+Scotland
+12	 
+4.	
+Mohamed Salah
+
+Liverpool
+
+Egypt
+10	 
+4.	
+David Silva
 -	
 
-Algeria
-11	 
-18.	
-Teemu Pukki
--	
-
-Finland
-11	 
-18.	
+Spain
+10	 
+4.	
 Son Heung-Min
 
 Tottenham Hotspur
 
 South Korea
-11 
+10	 
+7.	
+Riyad Mahrez
+-	
+
+Algeria
+9	 
+7.	
+Adama Traoré
+-	
+
+Spain
+9	 
+9.	
+Roberto Firmino
+-	
+
+Brazil
+8	 
+9.	
+Harvey Barnes
+-	
+
+England
+8	 
+11.	
+Willian
+-	
+
+Brazil
+7	 
+11.	
+Bruno Fernandes
+
+Manchester United
+
+Portugal
+7	 
+11.	
+Emiliano Buendía
+-	
+
+Argentina
+7	 
+11.	
+Gabriel Jesus
+-	
+
+Brazil
+7	 
+11.	
+Lucas Digne
+-	
+
+France
+7	 
+11.	
+Sadio Mané
+-	
+
+Senegal
+7	 
+11.	
+Bernardo Silva
+
+Manchester City
+
+Portugal
+7	 
+11.	
+Marcus Rashford
+
+Manchester United
+
+England
+7	 
+19.	
+César Azpilicueta
+-	
+
+Spain
+6	 
+19.	
+João Moutinho
+-	
+
+Portugal
+6	 
 """
-def parse_raw_data(raw, year="2023/2024", statName="Goals"):
+def parse_raw_data(raw, year="2019/2020", statName="Assists"):
     entries = re.split(r'\d+\.\s+', raw)[1:]  # Split by rank numbers
     data = []
     for entry in entries:
@@ -148,7 +152,7 @@ def parse_raw_data(raw, year="2023/2024", statName="Goals"):
     return data
 
 # Write data to CSV
-def write_to_csv(data, filename="output.csv", statName="Goals"):
+def write_to_csv(data, filename="output.csv", statName="Assists"):
     for rank, item in enumerate(data, start=1):
         item["Rank"] = rank
 
@@ -162,6 +166,6 @@ def write_to_csv(data, filename="output.csv", statName="Goals"):
             writer.writeheader()
         writer.writerows(data)
 
-structured_data = parse_raw_data(raw_data, year="2019/2020")
-write_to_csv(structured_data, filename="players_statstest.csv")
-print("Data written to players_stats.csv")
+structured_data = parse_raw_data(raw_data, year="2010/2020")
+write_to_csv(structured_data, filename="assists_stats.csv")
+print("Data written to assists_stats.csv")
